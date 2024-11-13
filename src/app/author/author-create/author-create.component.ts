6a622c60db25fc4a095252aa600e35f505ba9411
gorozcoua <g.orozcos@uniandes.edu.co>
2024-11-13 00:02:09 -0500

    Form init

---
+++ src/app/author/author-create/author-create.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-author-create',
+  templateUrl: './author-create.component.html',
+  styleUrls: ['./author-create.component.css']
+})
+export class AuthorCreateComponent implements OnInit {
+
+  authorForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.authorForm = this.formBuilder.group({
+      name: ["", [Validators.required, Validators.minLength(2)]],
+      image: ["", Validators.required],
+      birthDate: ["", Validators.required],
+      description: ["", [Validators.required, Validators.maxLength(100)]]
+    })
+  }
+
+}
